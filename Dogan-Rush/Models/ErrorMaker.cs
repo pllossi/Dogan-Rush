1b977608106c3886e552a84fb2fb3091d82e335f
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-04-30 11:41:28 +0200

    reso ErrorMaker come static class

--- Dogan-Rush/Models/ErrorMaker.cs
+++ Dogan-Rush/Models/ErrorMaker.cs
@@ -5,32 +5,42 @@ using System.Text;
 
 namespace Dogan_Rush.Models
 {
-    public class ErrorMaker
+    public static class ErrorMaker
     {
-        public ErrorMaker()
-        {
-            throw new System.NotImplementedException();
-        }
 
-        public VISACard VISACard
+        public static void GenerateError(VISACard visa)
         {
-            get => default;
-            set
+            Random rnd = new Random();
+            int errorPos = rnd.Next(0, 9);
+
+            switch (errorPos)
             {
-            }
-        }
+                case 0:
+                    //cambiare birthdate, necessario PersonData
+                    break;
+                case 1:
+                    //cambiare emissionDate, il tempo è di 5 anni.
+                    break;
+                case 2:
+                    //cambiare expirationDate, il tempo è di 5 anni
+                    break;
+                case 3:
+                    //cambiare Nome, cambiarlo totalmente o un solo char
+                    break;
+                case 4:
+                    //cambiare PassportCode, cambiarlo di un solo char
+                    break;
+                case 5:
+                    //cambiare lo stato emettitore
+                    break;
+                case 6:
+                    //cambiare il sesso segnato
+                    break;
+                default:
+                    //cambiare il cognome, totalmente o un solo char.
+                    break;
 
-        public IDCard IDCard
-        {
-            get => default;
-            set
-            {
             }
-        }
-
-        public void generateError()
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
