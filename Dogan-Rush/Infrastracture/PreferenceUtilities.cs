10371fb4ede1708cfce74a7d5197bb0a79aab76e
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-05-10 08:24:46 +0200

    viewModel iniziato

---
+++ Dogan-Rush/Infrastracture/PreferenceUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Dogan_Rush.Models;
+
+namespace Dogan_Rush.Infrastracture
+{
+    public static class PreferenceUtilities
+    {
+
+        private static readonly JsonSerializerOptions _defaultJsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        public static Person GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SavePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- Dogan-Rush/View-Models/VISACardViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Dogan_Rush.View_Models
-{
-    public partial class VISACardViewModel : ObservableObject
-    {
-    }
-}
